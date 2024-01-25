d0f734c7ec041160c4745f4bd4efb87dc997568c
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-01-25 10:48:58 +0200

    Add figcaption element for author data and update array

--- 01_randomQuoteMachine/01_randomQuoteMachine.js
+++ 01_randomQuoteMachine/01_randomQuoteMachine.js
@@ -6,40 +6,44 @@ class QuoteComponent extends React.Component {
       const quotes = [
     {
       text: '...in spite of everything, I still believe that people are really good at heart.',
-      author: '- Anne Frank'
+      author: 'Anne Frank'
     },
     {
       text: 'Always code as if the guy who ends up maintaining your cod will be a violent psychopath who knows where you live.',
-      author: '- John Woods'
+      author: 'John Woods'
     },
     {
       text: 'Truth can only be found in one place: the code.',
-      author: '- Robert C. Martin'
+      author: 'Robert C. Martin'
     },
     {
       text: 'A bend in the road is not the end of the road - unless you fail to make the turn.',
-      author: '- Hellen Keller'
+      author: 'Hellen Keller'
     },
     {
       text: 'If we all did the things we are capable of doing, we would literally astound ourselves',
-      author: '- Thomas Alva Edison'
+      author: 'Thomas Alva Edison'
     },
     {
       text: 'The only way to do great work is to do what you love.  If you haven\'t found it yet,  keep looking, Don\'t settle.',
-      author: '- Steve Jobs'
+      author: 'Steve Jobs'
     },
     {
       text: 'It is a profound and necessary truth that the deep things in science are not found because they are useful; they are found because it was possible to find them.',
-      author: '- Robert Oppenheimer'
+      author: 'Robert Oppenheimer'
     }
   ];
       const index = Math.trunc(Math.random() * quotes.length);
       return (
-        <div className="container w-50 p-4 bg-secondary text-white">
-          <blockquote class="blockquote">
+        <div className="container w-50 p-4 bg-dark text-white">
+          <figure>
+            <blockquote class="blockquote">
             <p id='text'>{quotes[index].text}</p>
-          </blockquote>
-          <p id='author'>{quotes[index].author}</p>        
+            </blockquote>
+            <figcaption class="blockquote-footer d-flex justify-content-end fst-italic text-white">
+              <p id='author'>{quotes[index].author}</p> 
+            </figcaption>
+          </figure>
           <a href='twitter.com/intent/tweet' id='tweet-quote'>
             <button type='button' className='btn btn-secondary btn-lg'id='new-quote'>Twitter</button>
           </a>
