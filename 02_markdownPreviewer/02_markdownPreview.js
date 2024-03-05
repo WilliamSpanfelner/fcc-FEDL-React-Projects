b687cd3cdb7f46c11d5d5281cbf4bf8370d8a3cf
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-03-05 14:47:03 +0200

    Add some markup to render

--- 02_markdownPreviewer/02_markdownPreview.js
+++ 02_markdownPreviewer/02_markdownPreview.js
@@ -1,3 +1,7 @@
-class MarkdownPreviewerApp extends React.Components {
-
+class MarkdownPreviewerApp extends React.Component {
+    render() {
+        return (
+            <div>Hello</div>
+        );
+    }
 }
