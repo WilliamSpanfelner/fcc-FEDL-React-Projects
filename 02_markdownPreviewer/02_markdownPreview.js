3e4e008a39a2a11f4bb0b14eb9127066b448cf72
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-03-05 15:52:11 +0200

    Create component classes

--- 02_markdownPreviewer/02_markdownPreview.js
+++ 02_markdownPreviewer/02_markdownPreview.js
@@ -1,3 +1,25 @@
+class EditorComponent extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>Hello</div>
+        );
+    }
+}
+
+class PreviewComponent extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>World</div>
+        );
+    }
+}
+
 class MarkdownPreviewerApp extends React.Component {
     constructor(props) {
         super(props);
