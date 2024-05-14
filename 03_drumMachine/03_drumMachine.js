219cee3eea50720f47079e545ec13d45f5b06b89
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-05-14 21:57:36 +0300

    Group DrumPad components into 3 groups for styling

--- 03_drumMachine/03_drumMachine.js
+++ 03_drumMachine/03_drumMachine.js
@@ -48,15 +48,21 @@ class DrumMachine extends React.Component {
                 return;
             }}>
                 <div id="display">Audio Clip Name</div>
-                <DrumPad keyboardKey={'Q'} audioClipName={"Heater1"} audioSrc={audioSrcPrefix + "Heater-1.mp3"} onPadClick={() => this.handleClick('Q')} />
-                <DrumPad keyboardKey={'W'} audioClipName={"Heater2"} audioSrc={audioSrcPrefix + "Heater-2.mp3"} onPadClick={() => this.handleClick('W')} />
-                <DrumPad keyboardKey={'E'} audioClipName={"Heater3"} audioSrc={audioSrcPrefix + "Heater-3.mp3"} onPadClick={() => this.handleClick('E')} />
-                <DrumPad keyboardKey={'A'} audioClipName={"Heater4"} audioSrc={audioSrcPrefix + "Heater-4_1.mp3"} onPadClick={() => this.handleClick('A')} />
-                <DrumPad keyboardKey={'S'} audioClipName={"Clap"} audioSrc={audioSrcPrefix + "Heater-6.mp3"} onPadClick={() => this.handleClick('S')} />
-                <DrumPad keyboardKey={'D'} audioClipName={"Open-HH"} audioSrc={audioSrcPrefix + "Dsc_Oh.mp3"} onPadClick={() => this.handleClick('D')} />
-                <DrumPad keyboardKey={'Z'} audioClipName={"Kick-n'-Hat"} audioSrc={audioSrcPrefix + "Kick_n_Hat.mp3"} onPadClick={() => this.handleClick('Z')} />
-                <DrumPad keyboardKey={'X'} audioClipName={"Kick"} audioSrc={audioSrcPrefix + "RP4_KICK_1.mp3"} onPadClick={() => this.handleClick('X')} />
-                <DrumPad keyboardKey={'C'} audioClipName={"Closed-HH"} audioSrc={audioSrcPrefix + "Cev_H2.mp3"} onPadClick={() => this.handleClick('C')} />
+                <div className="drumPadRow">
+                    <DrumPad keyboardKey={'Q'} audioClipName={"Heater1"} audioSrc={audioSrcPrefix + "Heater-1.mp3"} onPadClick={() => this.handleClick('Q')} />
+                    <DrumPad keyboardKey={'W'} audioClipName={"Heater2"} audioSrc={audioSrcPrefix + "Heater-2.mp3"} onPadClick={() => this.handleClick('W')} />
+                    <DrumPad keyboardKey={'E'} audioClipName={"Heater3"} audioSrc={audioSrcPrefix + "Heater-3.mp3"} onPadClick={() => this.handleClick('E')} />
+                </div>
+                <div className="drumPadRow">
+                    <DrumPad keyboardKey={'A'} audioClipName={"Heater4"} audioSrc={audioSrcPrefix + "Heater-4_1.mp3"} onPadClick={() => this.handleClick('A')} />
+                    <DrumPad keyboardKey={'S'} audioClipName={"Clap"} audioSrc={audioSrcPrefix + "Heater-6.mp3"} onPadClick={() => this.handleClick('S')} />
+                    <DrumPad keyboardKey={'D'} audioClipName={"Open-HH"} audioSrc={audioSrcPrefix + "Dsc_Oh.mp3"} onPadClick={() => this.handleClick('D')} />
+                </div>
+                <div className="drumPadRow">
+                    <DrumPad keyboardKey={'Z'} audioClipName={"Kick-n'-Hat"} audioSrc={audioSrcPrefix + "Kick_n_Hat.mp3"} onPadClick={() => this.handleClick('Z')} />
+                    <DrumPad keyboardKey={'X'} audioClipName={"Kick"} audioSrc={audioSrcPrefix + "RP4_KICK_1.mp3"} onPadClick={() => this.handleClick('X')} />
+                    <DrumPad keyboardKey={'C'} audioClipName={"Closed-HH"} audioSrc={audioSrcPrefix + "Cev_H2.mp3"} onPadClick={() => this.handleClick('C')} />
+                </div>
             </div>
         );
     }
