0e5530124dce9d19fa3071e7439a377de816514b
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-05-17 18:02:59 +0300

    Create a template parent component

---
+++ 04_jsCalculator/04_jsCalculator.js
@@ -0,0 +1,13 @@
+class AppName extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<AppName />, document.getElementById("app"));
