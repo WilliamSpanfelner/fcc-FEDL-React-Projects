fa7c76d7c751cb352b02e9cc5244e0107a80f70b
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-05-17 18:24:46 +0300

    Add remaining button objects

--- 04_jsCalculator/04_jsCalculator.js
+++ 04_jsCalculator/04_jsCalculator.js
@@ -3,7 +3,7 @@ class CalcButtons extends React.Component {
         super(props);
     }
     render() {
-        const buttons = [{id: "equals", label: "="}, {id: "zero", label: "0"}];
+        const buttons = [{id: "equals", label: "="}, {id: "zero", label: "0"}, {id: "one", label: "1"}, {id: "two", label: "2"}, {id: "three", label: "3"}, {id: "four", label: "4"}, {id: "five", label: "5"}, {id: "six", label: "6"}, {id: "seven", label: "7"}, {id: "eight", label: "8"}, {id: "nine", label: "9"}, {id: "add", label: "+"}, {id: "subtract", label: "-"}, {id: "multiply", label: "x"}, {id: "divide", label: "/"}, {id: "decimal", label: "."}, {id: "clear", label: "C"},];
         
         // https://react.dev/learn#rendering-lists
         const renderButtons = buttons.map(button =>
@@ -24,6 +24,7 @@ class AppName extends React.Component {
     render() {
         return (
             <div>
+                <CalcButtons />
             </div>
         )
     }
