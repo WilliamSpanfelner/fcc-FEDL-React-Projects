9c9b7235a31560f96f005c4f41899342626e5d4a
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-05-17 19:06:20 +0300

    Update parent component name and button label property values

--- 04_jsCalculator/04_jsCalculator.js
+++ 04_jsCalculator/04_jsCalculator.js
@@ -16,7 +16,7 @@ class CalcButtons extends React.Component {
         super(props);
     }
     render() {
-        const buttons = [{id: "equals", label: "="}, {id: "zero", label: "0"}, {id: "one", label: "1"}, {id: "two", label: "2"}, {id: "three", label: "3"}, {id: "four", label: "4"}, {id: "five", label: "5"}, {id: "six", label: "6"}, {id: "seven", label: "7"}, {id: "eight", label: "8"}, {id: "nine", label: "9"}, {id: "add", label: "+"}, {id: "subtract", label: "-"}, {id: "multiply", label: "x"}, {id: "divide", label: "/"}, {id: "decimal", label: "."}, {id: "clear", label: "C"},];
+        const buttons = [{id: "equals", label: "="}, {id: "zero", label: "0"}, {id: "one", label: "1"}, {id: "two", label: "2"}, {id: "three", label: "3"}, {id: "four", label: "4"}, {id: "five", label: "5"}, {id: "six", label: "6"}, {id: "seven", label: "7"}, {id: "eight", label: "8"}, {id: "nine", label: "9"}, {id: "add", label: "＋"}, {id: "subtract", label: "－"}, {id: "multiply", label: "×"}, {id: "divide", label: "÷"}, {id: "decimal", label: "."}, {id: "clear", label: "C"},];
         
         // https://react.dev/learn#rendering-lists
         const renderButtons = buttons.map(button =>
@@ -30,7 +30,7 @@ class CalcButtons extends React.Component {
     }
 }
 
-class AppName extends React.Component {
+class Calculator extends React.Component {
     constructor(props) {
         super(props);
     }
@@ -44,4 +44,4 @@ class AppName extends React.Component {
     }
 }
 
-ReactDOM.render(<AppName />, document.getElementById("app"));
+ReactDOM.render(<Calculator />, document.getElementById("app"));
