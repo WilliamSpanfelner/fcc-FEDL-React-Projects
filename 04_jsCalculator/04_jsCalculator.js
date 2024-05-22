a22f11e587e9bc580201a83017f6263bbfebfdaf
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-05-22 14:29:15 +0300

    Rename buttons map and add onClick attribute

--- 04_jsCalculator/04_jsCalculator.js
+++ 04_jsCalculator/04_jsCalculator.js
@@ -17,13 +17,13 @@ class CalcButtons extends React.Component {
         const buttons = [{id: "equals", label: "="}, {id: "zero", label: "0"}, {id: "one", label: "1"}, {id: "two", label: "2"}, {id: "three", label: "3"}, {id: "four", label: "4"}, {id: "five", label: "5"}, {id: "six", label: "6"}, {id: "seven", label: "7"}, {id: "eight", label: "8"}, {id: "nine", label: "9"}, {id: "add", label: "＋"}, {id: "subtract", label: "－"}, {id: "multiply", label: "×"}, {id: "divide", label: "÷"}, {id: "decimal", label: "."}, {id: "clear", label: "C"},];
         
         // https://react.dev/learn#rendering-lists
-        const renderButtons = buttons.map(button =>
-            <button key={button.id} id={button.id} >
+        const renderKeypad = buttons.map(button =>
+            <button key={button.id} id={button.id} onClick={this.props.execute}>
                 {button.label}
             </button>
         );
         return(
-            <div>{renderButtons}</div>
+            <div>{renderKeypad}</div>
         );
     }
 }
