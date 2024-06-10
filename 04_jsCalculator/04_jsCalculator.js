3ad15c84a60af788add8bfcbbdaeef46674b6961
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-06-10 11:30:47 +0300

    Update saveOperand parameter name and refactor

--- 04_jsCalculator/04_jsCalculator.js
+++ 04_jsCalculator/04_jsCalculator.js
@@ -105,22 +105,24 @@ class Calculator extends React.Component {
         })
     }
     // saveOperand will set state based on desired operation and operands available
-    saveOperand(op) {
-        const operator = op.id;
+    saveOperand(operator) {
+        // const operator = op.id;
         this.setState(prevState => {
             const updateOperand = Number(this.state.input);
-            if (prevState.operation && prevState.operand1 && operator === 'equals') {
+            if (prevState.operation && prevState.operand1 && operator.id === 'equals') {
                 return {
                     output: this.calculate[`${prevState.operation}`](Number(prevState.operand1), Number(updateOperand)),
                     operand2: updateOperand,
+                    expression: prevState.expression + operator.innerText,
                 }
             }
             return {
                 input: '0',
                 output: prevState.output,
-                operation: operator != "equals" ? operator : prevState.operation,
-                operand1: operator != "equals" ? prevState.input : prevState.operand1,
-                operand2: op.id === "equals" ? updateOperand : prevState.operand2,
+                operation: operator.id != "equals" ? operator.id : prevState.operation,
+                operand1: operator.id != "equals" ? prevState.input : prevState.operand1,
+                operand2: operator.id === "equals" ? updateOperand : prevState.operand2,
+                expression: prevState.expression + operator.innerText,
             }
         });
     }
